ee0f0bb5fc17d6a5b00f338a0ecca209660384ce
greybutton <greybutton@yandex.com>
2020-07-24 11:27:21 +0400

    add state to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,22 @@
 
 class User < ApplicationRecord
   extend Enumerize
+  include AASM
 
   enumerize :role, in: %i[user moderator legal], default: :user, predicates: true
+
+  aasm :state, column: :state do
+    state :permitted, initial: true
+    state :banned
+
+    event :ban do
+      transitions from: %i[permitted], to: :banned
+    end
+
+    event :unban do
+      transitions from: %i[banned], to: :permitted
+    end
+  end
 
   def username=(value)
     super(value.mb_chars.downcase)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_070804) do
+ActiveRecord::Schema.define(version: 2020_07_24_072652) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_07_24_070804) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "profession"
     t.string "workplace"
+    t.string "state"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
